e96c942056c0a611cadafcd8ca68fde65b46b73b
Kim Burgestrand <kim@burgestrand.se>
2013-06-25 11:39:11 +0200

    Add specs for Source#playing? and Source#stopped?

--- spec/plaything/source_spec.rb
+++ spec/plaything/source_spec.rb
@@ -1,7 +1,7 @@
 describe Plaything::OpenAL::Source do
-  describe "#starved?" do
-    subject(:source) { Plaything::OpenAL::Source.new(1337) }
+  subject(:source) { Plaything::OpenAL::Source.new(1337) }
 
+  describe "#starved?" do
     it "returns true if source should be playing but isnt" do
       Plaything::OpenAL.should_receive(:source_play).with(source)
       source.should_receive(:playing?).and_return(false)
@@ -22,6 +22,35 @@ describe Plaything::OpenAL::Source do
 
     it "returns false if source should not be playing and isnt" do
       source.should_not be_starved
+    end
+  end
+
+  describe "#playing?" do
+    it "returns true when playing" do
+      source.should_receive(:state).and_return(:playing)
+      source.should be_playing
+    end
+
+    it "returns false when not playing" do
+      source.should_receive(:state).and_return(:stopped)
+      source.should_not be_playing
+    end
+  end
+
+  describe "#stopped?" do
+    it "returns true when stopped" do
+      source.should_receive(:state).and_return(:stopped)
+      source.should be_stopped
+    end
+
+    it "returns false when not stopped" do
+      source.should_receive(:state).and_return(:paused)
+      source.should_not be_stopped
+    end
+
+    it "returns false when not stopped" do
+      source.should_receive(:state).and_return(:playing)
+      source.should_not be_stopped
     end
   end
 end
